a7b6765632326c9eaa9065eff36a8d0ac1649510
Tempo <pagan.emma@gmail.com>
2016-04-25 16:54:40 +0200

    Update IhmGroupe.java
    modif sur l'ihm du groupe

--- m1109_tp2/src/src/m1109/ihm/IhmGroupe.java
+++ m1109_tp2/src/src/m1109/ihm/IhmGroupe.java
@@ -3,6 +3,14 @@
  */
 package m1109.ihm;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import m1109.nf.Contact;
 import m1109.nf.Groupe;
 /**
  *
@@ -12,9 +20,12 @@ public class IhmGroupe extends javax.swing.JPanel {
     /*
      * Composants graphiques constituants l'interface
      */
-    private IhmLogo ihmLogo;
-    
-    
+    private IhmLogo ihmLogo, logo;
+    private JTextField champNomG;
+    private JTable table;
+    private JButton effacer, valider, annuler;
+    private JList symbole; 
+    private DefaultTableModel model;
     /**
      * Creates new form CarnetUI
      */
@@ -34,12 +45,45 @@ public class IhmGroupe extends javax.swing.JPanel {
     private void initUIComponents() {      
         
         this.add(new javax.swing.JLabel("Fiche groupe à compléter"));
-                
-        ihmLogo = new IhmLogo();
-        this.add(ihmLogo);  
         
-        // A Compléter en TP2
-    }
+        // TP2 - 4.1
+        this.add(new JLabel("Nom du groupe :"));
+        champNomG = new JTextField(30);
+        this.add(champNomG);
+        
+        // Table des contacts
+        String [] colonnes = {"Nom", "Prénom", "Date de naiss"};
+        
+        model = new DefaultTableModel();
+        model.setColumnIdentifiers(colonnes); //intitulé des colonnes        
+        JTable table = new JTable(model);       
+        this.add(table.getTableHeader());
+        this.add(table);
+        
+        // Zone de dessin
+        ihmLogo = new IhmLogo();
+        this.add(ihmLogo); 
+        
+        // Bouton pour effacer zone de dessin
+        effacer = new JButton("Effacer");
+        this.add(effacer);
+        
+        // Liste des symboles associés au groupe
+        this.add(new JLabel("Symbole :"));
+        symbole = new JList();
+        this.add(symbole);
+        
+        
+        // Valider les modifications
+        valider = new JButton("Valider");
+        this.add(valider);
+    
+        // Annuler les modifications
+        annuler = new JButton("Annuler");
+        this.add(annuler);
+    }        
+                         
+
     
     /**
      * Affecte les valeurs des attributs d'un groupe aux widgets de l'IHM
@@ -47,7 +91,15 @@ public class IhmGroupe extends javax.swing.JPanel {
      */    
     public void displayGroupe(Groupe groupe) {
         if (groupe != null) { 
-            // A Compléter en TP2
+            // TP2 - 4.2
+            
+            for(Contact c : groupe.getContacts()){  
+                String [] valeur = {c.getNom(), c.getPrenom(), c.getDateNaissance()};
+                
+                for(int i=0; i<valeur.length; i++){
+                   model.addRow(valeur);
+                }
+            }          
         }
     }
     
